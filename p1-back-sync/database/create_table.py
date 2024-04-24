911230d523f02a9ff3ae1bb188403c9620d13284
Emanuele Morais <emanuele.lmorais@gmail.com>
2024-04-24 01:12:27 -0300

    p1

---
+++ p1-back-sync/database/create_table.py
@@ -0,0 +1,8 @@
+import sqlite3
+con = sqlite3.connect("sqlite.db")
+cur = con.cursor()
+
+cur.execute("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)")
+
+con.commit()
+con.close()
